4d1cdebbb2f6a006265c7f9e6218eef71e7a0b9c
santi-grau <santi@proper-code.com>
2014-11-03 14:11:26 +0100

    added shader

--- public/js/city2.js
+++ public/js/city2.js
@@ -15,7 +15,6 @@ var Noise = Backbone.Model.extend({
 		});
 		var p = perm = [];
 		for (var i=0; i<256; i++) p[i] = perm[i] = perm[i+256] = Math.floor(this.random()*256);
-		console.log(perm.join(','))
 		this.set('p', p);
 		this.set('perm', perm);
 	},
@@ -79,12 +78,13 @@ var Noise = Backbone.Model.extend({
 	}
 })
 
-var p = new Noise({ seed : 1 });
+
+
+
+var p = new Noise({ });
 // Dimensions of the landscape.
-var tilesHeight = 200;
-var tilesWidth = 200;
-
-
+var tilesHeight = 60;
+var tilesWidth = 60;
 // Gather our HTML template as an array of strings for output.
 var html = [];
 html.push("<table>");
@@ -92,6 +92,7 @@ for (var y = 0; y < tilesHeight; y++) {
     html.push("<tr>");
     
     for (var x = 0; x < tilesWidth; x++) {
+    	p.noise(x,y);
         var detail = Math.abs(p.noise(x / 300.0, y / 300.0));
         var desert1 = -p.noise(x / 100.0, y / 100.0);
         var desert2 = p.noise(x / 10.0, y / 10.0);
@@ -132,3 +133,109 @@ for (var y = 0; y < tilesHeight; y++) {
 }
 html.push("</table>");
 document.getElementById("map").innerHTML += html.join("");
+
+
+
+
+
+var fragment, vertex;
+jQuery.ajax({ async:false, dataType: "text", url:"js/shaders/fragment.glsl", success:function(data){ fragment = data; } });
+jQuery.ajax({ async:false, dataType: "text", url:"js/shaders/vertex.glsl", success:function(data){ vertex = data; } });
+//console.log(fragment)
+// console.log(vertex)
+
+
+window.requestAnimationFrame = window.requestAnimationFrame || ( function() {
+	return  window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+	function(  callback, element ) {
+		window.setTimeout( callback, 1000 / 60 );
+	};
+})();
+
+var canvas;
+var gl;
+var buffer;
+var vertex_shader;
+var fragment_shader;
+var currentProgram;
+var vertex_position;
+
+var parameters = {
+	start_time  : new Date().getTime(), 
+	time        : 0, 
+	screenWidth : 0, 
+	screenHeight: 0
+};
+
+init();
+animate();
+function init() {
+	vertex_shader = vertex;
+	fragment_shader = fragment;
+	canvas = document.querySelector( 'canvas' );
+	try { gl = canvas.getContext( 'experimental-webgl' ); } catch( error ) { }
+	if ( !gl ) throw "cannot create webgl context";
+	buffer = gl.createBuffer();
+	gl.bindBuffer( gl.ARRAY_BUFFER, buffer );
+	gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( [ - 1.0, - 1.0, 1.0, - 1.0, - 1.0, 1.0, 1.0, - 1.0, 1.0, 1.0, - 1.0, 1.0 ] ), gl.STATIC_DRAW );
+	currentProgram = createProgram( vertex_shader, fragment_shader );
+	onWindowResize();
+	window.addEventListener( 'resize', onWindowResize, false );
+}
+function createProgram( vertex, fragment ) {
+	var program = gl.createProgram();
+	var vs = createShader( vertex, gl.VERTEX_SHADER );
+	var fs = createShader( '#ifdef GL_ES\nprecision highp float;\n#endif\n\n' + fragment, gl.FRAGMENT_SHADER );
+	if ( vs == null || fs == null ) return null;
+	gl.attachShader( program, vs );
+	gl.attachShader( program, fs );
+	gl.deleteShader( vs );
+	gl.deleteShader( fs );
+	gl.linkProgram( program );
+	if ( !gl.getProgramParameter( program, gl.LINK_STATUS ) ) {
+		alert( "ERROR:\n" +
+		"VALIDATE_STATUS: " + gl.getProgramParameter( program, gl.VALIDATE_STATUS ) + "\n" +
+		"ERROR: " + gl.getError() + "\n\n" +
+		"- Vertex Shader -\n" + vertex + "\n\n" +
+		"- Fragment Shader -\n" + fragment );
+		return null;
+	}
+	return program;
+}
+function createShader( src, type ) {
+	var shader = gl.createShader( type );
+	gl.shaderSource( shader, src );
+	gl.compileShader( shader );
+	if ( !gl.getShaderParameter( shader, gl.COMPILE_STATUS ) ) {
+		alert( ( type == gl.VERTEX_SHADER ? "VERTEX" : "FRAGMENT" ) + " SHADER:\n" + gl.getShaderInfoLog( shader ) );
+		return null;
+	}
+	return shader;
+}
+function onWindowResize( event ) {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	parameters.screenWidth = canvas.width;
+	parameters.screenHeight = canvas.height;
+	gl.viewport( 0, 0, canvas.width, canvas.height );
+}
+function animate() {
+	requestAnimationFrame( animate );
+	render();
+}
+function render() {
+	if ( !currentProgram ) return;
+		parameters.time = new Date().getTime() - parameters.start_time;
+		gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
+		// Load program into GPU
+		gl.useProgram( currentProgram );
+		// Set values to program variables
+		gl.uniform1f( gl.getUniformLocation( currentProgram, 'time' ), parameters.time / 1000 );
+		gl.uniform2f( gl.getUniformLocation( currentProgram, 'resolution' ), parameters.screenWidth, parameters.screenHeight );
+		// Render geometry
+		gl.bindBuffer( gl.ARRAY_BUFFER, buffer );
+		gl.vertexAttribPointer( vertex_position, 2, gl.FLOAT, false, 0, 0 );
+		gl.enableVertexAttribArray( vertex_position );
+		gl.drawArrays( gl.TRIANGLES, 0, 6 );
+		gl.disableVertexAttribArray( vertex_position );
+	}
